09f0a88c2cfe2682c5f625f3a14215a922de4bf6
thomaswdaniel <thomaswdaniel@icloud.com>
2023-02-03 16:36:28 +0000

    fixing page load

--- client/azure-mern-client/src/App.js
+++ client/azure-mern-client/src/App.js
@@ -1,86 +1,30 @@
-/*
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Heading">
+      <h1>Sign Up</h1>
+      <p>Please fill in the below information</p>
+      
+      <div class="masonry-grid">
+      <input type="text" placeholder="Enter Username" name="Username" required></input>
+      <div class="masonry-grid__item">...</div>
+      
+      <input type="text" placeholder="Enter Password" name="Password" required></input>
+      <div class="masonry-grid__item">...</div>
+      
+      <input type="text" placeholder="Repeat Password" name="Password" required></input>
+      <div class="masonry-grid__item">...</div>
+      
+      <input type="checkbox" id="accept" name="Remember" value="yes" />  
+      
+      <label for="Remember"> Remember Me </label>
+      </div>
+
+      
     </div>
+   
   );
 }
-
-export default App;
-
-*/
-import React from "react";
-import axios from 'axios';
-import "./index.css";
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bestShows: []
-    };
-  }
-/*
-  componentDidMount() { //dev
-    axios.get('http://localhost:5000/api/data')
-      .then(res => {
-        console.log("data recieved: ", res.data[0]);
-        this.setState({ bestShows: res.data[0] });
-      })
-      .catch(alert);
-  }
-*/
-  
-      componentDidMount() { //prod
-          axios.get('/api/data')
-              .then(res => {
-                  console.log("data recieved: ", res.data[0]);
-                  this.setState({ bestShows: res.data[0] });
-              })
-              .catch(alert);
-      }
-  
-
-  render() {
-    return (
-      <div>
-        testtesttesttesttest
-        <ul>
-          {
-            Object.keys(this.state.bestShows).map((cur, idx) => (
-              <li>{cur} - {this.state.bestShows[cur]} </li>
-            ))
-          }
-
-        </ul>
-      </div>
-    );
-  }
-}
-
-//const rootElement = document.getElementById("root");
-//ReactDOM.render(<WorldMap />, rootElement);
-
 
 export default App;
