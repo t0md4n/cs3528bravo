1a1363f84796f9449b773f9c6fd79b4cf5f7d859
ReeceMusgraveUni <91484752+ReeceMusgraveUni@users.noreply.github.com>
2023-02-03 15:33:38 +0000

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,31 @@
-import React from "react";
-import axios from 'axios';
-import "./index.css";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bestShows: []
-    };
-  }
+import './App.css';
 
-  componentDidMount() {
-    console.log("componentDidMount success")
-    axios.get('/api/data')
-      .then(res => {
-        console.log("data recieved: ", res.data);
-        this.setState({ bestShows: res.data[0] });
-      })
-      .catch(alert);
-  }
-
-
-  render() {
-    console.log("render bestShows: ", this.state.bestShows)
-    return (
-      <div>
-        azure-mern-demo
-        <ul>
-          {
-            Object.keys(this.state.bestShows).map((cur, idx) => (
-              <li>{cur} - {this.state.bestShows[cur]} </li>
-            ))
-          }
-        </ul>
+function App() {
+  return (
+    <div className="Heading">
+      <h1>Sign Up</h1>
+      <p>Please fill in the below information</p>
+      
+      <div class="masonry-grid">
+      <input type="text" placeholder="Enter Username" name="Username" required></input>
+      <div class="masonry-grid__item">...</div>
+      
+      <input type="text" placeholder="Enter Password" name="Password" required></input>
+      <div class="masonry-grid__item">...</div>
+      
+      <input type="text" placeholder="Repeat Password" name="Password" required></input>
+      <div class="masonry-grid__item">...</div>
+      
+      <input type="checkbox" id="accept" name="Remember" value="yes" />  
+      
+      <label for="Remember"> Remember Me </label>
       </div>
-    );
-  }
+
+      
+    </div>
+   
+  );
 }
 
 export default App;
