a2cd4aa32512b0439b2d6a0ce32f896c1d1c1014
rahias20 <r.mohammad.20@abdn.ac.uk>
2023-03-13 19:56:44 +0000

    navbar changed

--- client/src/pages/Navbar.page.js
+++ client/src/pages/Navbar.page.js
@@ -1,27 +1,15 @@
 import '../components/css/navbar.css'
 import React from 'react';
-import { Link } from "react-router-dom";
+
 
  const Navbar = () => {
   return (
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/events">Events</Link>
-          </li>
-          <li>
-            <Link to="/createevent">Create</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-        </ul>
+          <a href="/">Home</a>
+          <a href="/events">Events</a>
+          <a href="/createevent">Create</a>
+          <a href="/login">Login</a>
+          <a href="/signup">Sign Up</a>
       </nav>
     );
  }
