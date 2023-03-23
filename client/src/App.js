f35b9fe90f545e749a3c1e33f67769194ebba2c1
thomaswdaniel <thomaswdaniel@icloud.com>
2023-03-23 22:44:15 +0000

    testing fix for signup

--- client/src/App.js
+++ client/src/App.js
@@ -28,13 +28,13 @@ function App() {
       <UserProvider>
         <Routes>
           <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-          <Route exact path="/events" element={<Events />} />
-          <Route exact path="/createevent" element={<CreateEvent/> } />
+          <Route exact path="/signup" element={<Signup />} />>
           {/* We are protecting the create event and event page from unauthenticated */}
           {/* users by wrapping it with PrivateRoute here. */}
           <Route element={<PrivateRoute />}>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/events" element={<Events />} />
+            <Route exact path="/createevent" element={<CreateEvent/> } />
           </Route>
         </Routes>
       </UserProvider>
