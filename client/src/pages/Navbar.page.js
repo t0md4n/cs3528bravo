e87f5fe1b43927925f767f87712f1e0490a22798
thomaswdaniel <thomaswdaniel@icloud.com>
2023-03-25 21:45:36 +0000

    added dropdown for events in navbar

--- client/src/pages/Navbar.page.js
+++ client/src/pages/Navbar.page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 // import { Link } from "react-router-dom";
 // import Button from 'react-bootstrap/Button';
 // import '../components/css/navbar.css'
@@ -18,8 +18,12 @@ function NavigationBar() {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/events">Events</Nav.Link>
-                <Nav.Link href="/createevent">Create Event</Nav.Link>
+                <NavDropdown title="Events" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/createevent">Create Event</NavDropdown.Item>
+                  <NavDropdown.Item href="/events">Current Events</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/myevents">My Events</NavDropdown.Item>
+                </NavDropdown>
                 <Nav.Link href="/login">Login</Nav.Link>
                 <Nav.Link href="/signup">Sign up</Nav.Link>
               </Nav>
