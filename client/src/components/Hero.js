0d4e04367fb9e930257d0db232bbeb0c4997041e
Alexander Brander <sandy.brander11@gmail.com>
2023-04-26 15:09:54 +0100

    Main push and homepage extras

---
+++ client/src/components/Hero.js
@@ -0,0 +1,22 @@
+import "./css/heroStyles.css";
+
+function Hero(props) {
+    return (<>
+    <div className="props.cName">
+        <img alt="HeroImg" src={props.heroImg}/>
+        <div className="hero-text">
+            <h1>{props.title}</h1>
+            <p>{props.text}</p>
+            <div className="link">
+                <a href={props.url} className={props.btnClass}>
+                    {props.buttonText}
+                </a>
+            </div>
+            
+        </div>
+    </div>
+
+    </>);
+}
+
+export default Hero;
---
+++ client/src/pages/Footer.page.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../components/css/footer.css';
+
+const Footer=()=> {
+    return (
+        <div className="footer">
+            <div className="sb__footer section__padding">
+                <div className="sb__footer-below">
+                    <div className="sb__footer-copyright">
+                        <p>
+                            @{new Date().getFullYear()} Spòrs. All rights reserved.
+                        </p>
+                    </div>
+                    <div className="sb__footer-below-links">
+                        <a href="/ContactUs"><div><p>Contact Us</p></div></a>
+                        <a href="/Policy"><div><p>Terms and Conditions</p></div></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Footer;
